9b144e39bb113a487b23aa20f1d4f15ea8cea0cd
bespodobno <nadzeya.tron@gmail.com>
2019-03-20 20:49:24 +0300

    first ideas

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,15 +2,20 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null; // max element/node
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		const node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
+		if (this.isEmpty()) return;
 		
+
 	}
 
 	detachRoot() {
@@ -22,19 +27,26 @@ class MaxHeap {
 	}
 
 	size() {
-		
+		//return this.root.
 	}
 
 	isEmpty() {
-		
+		return this.root === null;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
+		if(this.isEmpty()){
+			this.root = (node);
+			this.parentNodes.push(node);
+			return;
+		}
 		
+
 	}
 
 	shiftNodeUp(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,47 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (this.left === null) {
+			this.left = node;
+			node.parent = this;
+		} else if (this.right === null) {
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left === node) {
+			this.left = null;
+			node.parent = null;
+			return;
+		} else if (this.right === node) {
+			this.right = null;
+			node.parent = null;
+			return;
+		}
+		throw Error("Nothing to remove")
 	}
 
 	remove() {
-
+		if (!this.parent) return;
+		this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+		//updates parent.parent
+		//updates parent.parent.parent
+		//updates child.parent
+		//updates parent.child.parent
+		//updates children of node and parent node
+
 	}
 }
 
